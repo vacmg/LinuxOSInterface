f4e6e5eff39a6b12bce62ce6ba1c8875d523c67e
vacmg <70143287+vacmg@users.noreply.github.com>
2025-03-18 20:33:47 +0100

    Update format

--- Source/include/LinuxOSInterface.h
+++ Source/include/LinuxOSInterface.h
@@ -6,13 +6,13 @@
 class LinuxOSInterface : public OSInterface
 {
 public:
-    void osSleep(uint32_t ms) override;
-    uint32_t osMillis() override;
-    OSInterface_Mutex* osCreateMutex() override;
+    void                         osSleep(uint32_t ms) override;
+    uint32_t                     osMillis() override;
+    OSInterface_Mutex*           osCreateMutex() override;
     OSInterface_BinarySemaphore* osCreateBinarySemaphore() override;
 
     void* osMalloc(uint32_t size) override;
-    void osFree(void* ptr) override;
+    void  osFree(void* ptr) override;
 };
 
 #endif // OSLINUXINTERFACE_H
